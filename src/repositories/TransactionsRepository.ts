4b09fbf3ed786528378563ae610b7e051ffbd339
Elias <eliasmelo1978@gmail.com>
2020-04-19 15:56:01 -0300

    Desafio concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,45 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    this.balance = { income: 0, outcome: 0, total: 0 };
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        this.balance.income += transaction.value;
+      } else {
+        this.balance.outcome += transaction.value;
+      }
+    });
+    this.balance.total = this.balance.income - this.balance.outcome;
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
